6a58f1143d0e7a7726f3bcc20726fa1f2c06d6fa
= <=>
2021-03-06 13:40:53 -0600

    improved methods

--- dbaccess_rest.py
+++ dbaccess_rest.py
@@ -1,5 +1,4 @@
 """ Autonomous JSON Database (AJD) access using REST APIs.
-
 Define a class and helper functions to store and retrieve data from
 a managed JSON database from Oracle. The APIs defined here use SODA
 commands exchanged using a REST protocol.
@@ -61,6 +60,7 @@ class JsonDatabase:
            raises an error. If the flag is False, the named collection should be available
            prior to instantiation.
     """
+
     def __init__(self, user, password, collection_name, create_coll=False):
         self.__user = user
         self.__pword = password
@@ -76,8 +76,10 @@ class JsonDatabase:
         """Create a new collection in the database"""
         all_colls = self.list_collections()
         if self.__collname in all_colls:
-            raise DatabaseCollectionExists("{} already exists".format(self.__collname))
-        resp = requests.put(url=self.__baseurl, auth=(self.__user, self.__pword))
+            raise DatabaseCollectionExists(
+                "{} already exists".format(self.__collname))
+        resp = requests.put(url=self.__baseurl,
+                            auth=(self.__user, self.__pword))
         if resp.status_code > 299:
             error_desc = str(resp.status_code) + " " + resp.reason
             raise DatabaseUnableToCreate(error_desc)
@@ -124,7 +126,8 @@ class JsonDatabase:
         json object in the database. Hence, the dictionary must be such that it can be translated
         into a json object. This method returns 1 if successful. Otherwise it raises an exception.
         """
-        resp = self.__make_post_request(target_url=self.__baseurl, payload_dict=item_dict)
+        resp = self.__make_post_request(
+            target_url=self.__baseurl, payload_dict=item_dict)
         if resp['status'] == "error":
             error_msg = str(resp.get("code")) + " " + resp.get("reason")
             raise DatabaseUnableToAddItem(message=error_msg)
@@ -147,7 +150,8 @@ class JsonDatabase:
             error_msg = "A collection can have only 1 metadata document."
             raise DatabaseMetadataAlreadyExists(error_msg)
 
-        resp = self.__make_post_request(target_url=self.__baseurl, payload_dict=formatted_dict)
+        resp = self.__make_post_request(
+            target_url=self.__baseurl, payload_dict=formatted_dict)
         if resp['status'] == "error":
             error_msg = str(resp.get("code")) + " " + resp.get("reason")
             raise DatabaseUnableToAddItem(message=error_msg)
@@ -157,7 +161,8 @@ class JsonDatabase:
     def extract_metadata(self):
         extract_url = self.__baseurl + "?action=query"
         qdata = {"type": {"$eq": "Metadata"}}
-        resp = self.__make_post_request(target_url=extract_url, payload_dict=qdata)
+        resp = self.__make_post_request(
+            target_url=extract_url, payload_dict=qdata)
 
         if resp['status'] == 'ok':
             returned_data = json.loads(resp.get("data"))
@@ -165,7 +170,8 @@ class JsonDatabase:
             count = returned_data.get("count")
 
             if count > 1:
-                error_msg = "Found {} metadata documents. Only one is allowed.".format(count)
+                error_msg = "Found {} metadata documents. Only one is allowed.".format(
+                    count)
                 raise DatabaseTooManyMetadataDocs(error_msg)
 
             elif count == 0:
@@ -184,7 +190,8 @@ class JsonDatabase:
         Returns 1 if successful. Otherwise it raises an error.
         """
         insert_url = self.__baseurl + "?action=insert"
-        resp = self.__make_post_request(target_url=insert_url, payload_dict=items_list)
+        resp = self.__make_post_request(
+            target_url=insert_url, payload_dict=items_list)
         if resp['status'] == "error":
             error_msg = str(resp.get("code")) + " " + resp.get("reason")
             raise DatabaseUnableToAddItem(message=error_msg)
@@ -193,67 +200,96 @@ class JsonDatabase:
 
     def extract_items(self, z_layer):
         """ Extract nuclear items at a given z layer"""
-        extract_url = self.__baseurl + "?action=query"
-        qdata = {"geometry.coordinates[2]": {"$eq": z_layer}}
-        resp = self.__make_post_request(target_url=extract_url, payload_dict=qdata)
-        if resp['status'] == 'ok':
-            returned_data = json.loads(resp.get("data"))
-            all_items = returned_data.get("items")
-            count = returned_data.get("count")
-            has_more = returned_data.get("hasMore")
+        offset = 0
+        has_more = True
+        items = []
+        while has_more:
+            extract_url = self.__baseurl + "?action=query&offset={}".format(offset)
+            qdata = {"geometry.coordinates[2]": {"$eq": z_layer}}
+            resp = self.__make_post_request(
+                target_url=extract_url, payload_dict=qdata)
+            if resp['status'] == 'ok':
+                returned_data = json.loads(resp.get("data"))
+                all_items = returned_data.get("items")
+                count = returned_data.get("count")
+                has_more = returned_data.get("hasMore")
+                offset += count
 
-            if has_more:
-                print("Database reports the return of only a fraction of all items. ")
-                print("This method has not been designed yet for very large data retrievals.")
-                sys.exit(0)
-
-            if count > 0:
-                extracted = [copy.deepcopy(it.get("value")) for it in all_items]
-                return extracted
+                if count > 0:
+                    extracted = [copy.deepcopy(it.get("value"))
+                                 for it in all_items]
+                    items += extracted
             else:
-                return []
-        else:
-            error_msg = str(resp.get("code")) + " " + resp.get("reason")
-            raise DatabaseUnableToExtractData(message=error_msg)
+                error_msg = str(resp.get("code")) + " " + resp.get("reason")
+                raise DatabaseUnableToExtractData(message=error_msg)
+        return items
 
     def extract_tile_data(self, z_layer, x0, y0, tile_size=256):
         """Extract nuclear data for a tile of a given size whose origin is at (x0, y0).
-
         Note: This function uses the $between comparison operator to check if a point belongs to
         a tile. We may want to replace this operation and use the $within operator for GeoJson data. It
         might be a faster way to perform queries.
         """
         xf = x0 + tile_size - 1
         yf = y0 + tile_size - 1
-        extract_url = self.__baseurl + "?action=query"
-        qdata = {"geometry.coordinates[2]": {"$eq": z_layer},
-                 "geometry.coordinates[0]": {"$between": [x0, xf]},
-                 "geometry.coordinates[1]": {"$between": [y0, yf]},
-                 }
+        offset = 0
+        has_more = True
+        items = []
+        while has_more:
+            extract_url = self.__baseurl + "?action=query&offset={}".format(offset)
+            qdata = {"geometry.coordinates[2]": {"$eq": z_layer},
+                     "geometry.coordinates[0]": {"$between": [x0, xf]},
+                     "geometry.coordinates[1]": {"$between": [y0, yf]},
+                     }
+            resp = self.__make_post_request(
+                target_url=extract_url, payload_dict=qdata)
+            if resp['status'] == 'ok':
+                returned_data = json.loads(resp.get("data"))
+                all_items = returned_data.get("items")
+                count = returned_data.get("count")
+                has_more = returned_data.get("hasMore")
+                offset += count
 
-        resp = self.__make_post_request(target_url=extract_url, payload_dict=qdata)
-        if resp['status'] == 'ok':
-            returned_data = json.loads(resp.get("data"))
-            all_items = returned_data.get("items")
-            count = returned_data.get("count")
-            has_more = returned_data.get("hasMore")
-
-            if has_more:
-                print("Warning: Only {} items extracted for tile with origin at ({}, {})".format(count, x0, y0))
-                print("         Database reports having more items. Consider reducing the tile size.")
-
-            if count > 0:
-                extracted = [copy.deepcopy(it.get("value")) for it in all_items]
-                return extracted
+                if count > 0:
+                    extracted = [copy.deepcopy(it.get("value"))
+                                 for it in all_items]
+                    items += extracted
             else:
-                return []
-        else:
-            error_msg = str(resp.get("code")) + " " + resp.get("reason")
-            raise DatabaseUnableToExtractData(message=error_msg)
+                error_msg = str(resp.get("code")) + " " + resp.get("reason")
+                raise DatabaseUnableToExtractData(message=error_msg)
+        return items
+
+    def extract_region(self, z_layer, x0, y0, xf, yf):
+        offset = 0
+        has_more = True
+        items = []
+        while has_more:
+            extract_url = self.__baseurl + "?action=query&offset={}".format(offset)
+            qdata = {"geometry.coordinates[2]": {"$eq": z_layer},
+                     "geometry.coordinates[0]": {"$between": [x0, xf]},
+                     "geometry.coordinates[1]": {"$between": [y0, yf]},
+                     }
+            resp = self.__make_post_request(
+                target_url=extract_url, payload_dict=qdata)
+            if resp['status'] == 'ok':
+                returned_data = json.loads(resp.get("data"))
+                all_items = returned_data.get("items")
+                count = returned_data.get("count")
+                has_more = returned_data.get("hasMore")
+                offset += count
+                if count > 0:
+                    extracted = [copy.deepcopy(it.get("value"))
+                                 for it in all_items]
+                    items += extracted
+            else:
+                error_msg = str(resp.get("code")) + " " + resp.get("reason")
+                raise DatabaseUnableToExtractData(message=error_msg)
+        return items
 
     def __make_post_request(self, target_url, payload_dict):
         # Note: the json argument automatically sets the content-type header to application/json
-        resp = requests.post(url=target_url, auth=(self.__user, self.__pword), json=payload_dict)
+        resp = requests.post(url=target_url, auth=(
+            self.__user, self.__pword), json=payload_dict)
         if 200 <= resp.status_code <= 299:
             return {"status": "ok", "code": resp.status_code, "data": resp.text}
         else:
@@ -267,7 +303,8 @@ class JsonDatabase:
             return {"status": "error", "code": resp.status_code, "reason": resp.reason}
 
     def __make_delete_request(self, target_url):
-        resp = requests.delete(url=target_url, auth=(self.__user, self.__pword))
+        resp = requests.delete(
+            url=target_url, auth=(self.__user, self.__pword))
         if resp.status_code == 200:
             return {"status": "ok", "code": resp.status_code}
         else:
